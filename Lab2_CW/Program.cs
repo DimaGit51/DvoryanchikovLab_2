c10dcbdfbbe63b4479387c5bb538ab9684c11616
didvo <didvoryanchikov@yandex.ru>
2023-02-21 11:23:25 +0400

    low

---
+++ Lab2_CW/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Lab2_CW
+{
+    //ВНИМАТЕЛЬНО ПИСАТЬ ФУНКЦИЮ И СОБЛЮДАТЬ ПРАВИЛО ПОСТОЯННОГО ВОЗРАСТАНИЯ ИЛИ УБЫВАНИЯ И ПРОХОЖДЕНИЯ ЧЕРЕЗ 0!
+    class Program
+    {
+        public static double f(double x)
+        {
+            return (x);
+        }
+        public static double function(double low, double high, double eps)
+        {
+            double z = (f(high) + f(low)) / 2;
+            if ((high - low) >= eps)
+            {
+                //if(f(low)*f(z)>0)
+                if(f(low)<0 && f(z)>0)
+                {
+                    z = function(low, z, eps);
+                }
+                if(f(z) < 0 && f(high) > 0)
+                {
+                    z = function(z, high, eps);
+                }
+            }
+            return z;
+        }
+        public static void Print()
+        {
+            double eps = 0.01;
+            Console.Write("Введите low: ");
+            double low;
+            while (!double.TryParse(Console.ReadLine(), out low))
+            {
+                Console.WriteLine("Вы ввели не число! Введите число!");
+            }
+            Console.Write("Введие high: ");
+            double high;
+            while (!double.TryParse(Console.ReadLine(), out high))
+            {
+                Console.WriteLine("Вы ввели не число! Введите число!");
+            }
+            if (low < high)
+            {
+                double x = function(low, high, eps);
+                Console.Write("Ответ: {0}",x);
+            }
+            
+
+
+
+        }
+
+    }
+}
---
+++ Lab2_CW/Tibet.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Lab2_CW
+{
+    //ВНИМАТЕЛЬНО ПИСАТЬ ФУНКЦИЮ И СОБЛЮДАТЬ ПРАВИЛО ПОСТОЯННОГО ВОЗРАСТАНИЯ ИЛИ УБЫВАНИЯ И ПРОХОЖДЕНИЯ ЧЕРЕЗ 0!
+    class Tibet
+    {
+        public static void function(int n, int A, int B, int C)
+        {
+            //2^n - 1
+            if (n == 1)
+            {
+                Console.WriteLine("A -> B");
+            }
+        }
+        public static void Main()
+        {
+            Console.Write("Введите кол-во дисков: ");
+            int n;
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("Вы ввели не число! Введите число!");
+            }
+            int A = 3;
+            int B = 0;
+            int C = 0;
+            function(n, A, B, C);
+        }
+
+    }
+}
